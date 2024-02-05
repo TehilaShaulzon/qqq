5dcce6cda70fd9b701f359462cfe2486ff44ebff
TehilaShaulzon <t8472959@gmail.com>
2024-02-05 23:08:13 +0200

    Middlewares

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 using Tasks.Services;
+using ToDo.Middlewares;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -11,6 +12,7 @@ builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
 
 var app = builder.Build();
+app.UselogMiddleware("file.txt");
 
 // Configure the HTTP request pipeline.
 if (app.Environment.IsDevelopment())
